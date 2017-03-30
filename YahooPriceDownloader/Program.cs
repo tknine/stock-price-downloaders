a35a288753c71f09b5e7e3f75cb430c180504e1d
Tom K <tom.klopfstein@gmail.com>
2017-03-30 13:46:43 -0700

    Completed yahoo downloader.

--- YahooPriceDownloader/Program.cs
+++ YahooPriceDownloader/Program.cs
@@ -8,100 +8,160 @@ using System.Net.Http;
 using System.IO;
 using MoreLinq;
 using System.Threading;
+using CommandLine;
+using LumenWorks.Framework.IO.Csv;
 
 namespace YahooPriceDownloader
 {
     class Program
     {
         private static SemaphoreSlim semaphore;
+        private static Options options;
+
+        private static Action<string> yahooDownloader = (string symbol) =>
+        {
+
+            Console.WriteLine($"Downloading: {symbol}");
+
+            //if multi-threaded, then have task wait on the semphore for a slot to run.
+            if (options.Threads > 1)
+            {
+                semaphore.Wait();
+            }
+            
+
+            var wc = new WebClient();
+
+            try
+            {
+                //download the data for the specified date range.
+                var data = wc.DownloadString(new Uri($@"https://ichart.finance.yahoo.com/table.csv"
+                    + $"?d={options.EndDate.Month - 1}"
+                    + $"&e={options.EndDate.Day}"
+                    + $"&f={options.EndDate.Year}"
+                    + $"&g=d"
+                    + $"&a={options.BeginDate.Month - 1}"
+                    + $"&b={options.BeginDate.Day}"
+                    + $"&c={options.BeginDate.Year}"
+                    + $"&ignore=.csv&s={symbol}"));
+
+                //parse the download and reverse the records to date ascending order
+                //The csv reader expects the input to be a stream, so use a extension method
+                //which will turn the download string into a StreamReader on a MemoryStream
+                using (var csv = new CachedCsvReader(data.ToStreamReader(), true))
+                {
+                    csv.ReadToEnd();
+
+                    csv.Records.Reverse();
+
+                    //output all the lines of the download using a Join of the fields sub-array to write out each line.
+                    File.WriteAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, options.DataFolder, $"{symbol}.csv"), csv.Records.Select(r => string.Join(",", r)));
+                }
+
+                Console.WriteLine($"Completed: {symbol}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error on: {symbol}{Environment.NewLine}{ex}");
+            }
+            finally
+            {
+                if (options.Threads > 1)
+                {
+                    //if multi-threaded, then release the semphore so that next task can run.
+                    semaphore.Release();
+                }
+            }
+        };
 
         static void Main(string[] args)
         {
-            Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data"));
+            ParserResult<Options> result;
+            bool isValidOptions = true;
 
-            semaphore = new SemaphoreSlim(3);
+            //map the CommandLine to the options object
+            result = CommandLine.Parser.Default.ParseArguments<Options>(args);
 
-            Action<string> a = (string symbol) =>
+            ParserResult<Options>  errorResult = result.WithNotParsed<Options>((IEnumerable<Error> errors) => {
+                foreach(var error in errors)
+                {
+                    Console.WriteLine(error);
+                }
+                isValidOptions = false;
+            });
+
+            //there were errors in the options so exit
+            if (!isValidOptions)
             {
+                Console.WriteLine("Invalid options specified.  Process terminated.");
+                Console.ReadKey();
+                return;
+            }
 
-                semaphore.Wait();
+            options = result.MapResult((Options opts) => opts, null);
+            
 
-                Console.WriteLine($"Downloading: {symbol}");
+            //create the data directory
+            Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, options.DataFolder));
 
-                var wc = new WebClient();
+            //get the symbols to download
+            List<string> symbols = new List<string>();
 
+            if (options.SymbolFile != null)
+            {
+                symbols = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, options.SymbolFile)).ToList();
+            }
+            else
+            {
+                symbols = options.Symbol.Split(" ".ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+            }
 
-                try
-                {
-                    var data = wc.DownloadString(new Uri($@"https://ichart.finance.yahoo.com/table.csv?d=3&e=31&f=2017&g=d&a=1&b=31&c=2016&ignore=.csv&s={symbol}"));
-                
-                    var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", $"{symbol}.csv");
+            //start the download with either a a single thread (current thread) or multi-threaded
+            if (options.Threads == 1)
+            {
+                DoWorkSingleThreaded(symbols, yahooDownloader);
+            }
+            else
+            {
+                semaphore = new SemaphoreSlim(options.Threads);
 
-                    Console.WriteLine($"Completed: {symbol}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"{ex.ToString()}: {symbol}");
-                }
+                DoWorkMultiThreaded(symbols, yahooDownloader);
+            }
 
-                semaphore.Release(1);
-            };
-
-            List<Action> actions = new List<Action>();
-
-            List<string> symbols = "AAPL NUGT DUST CSCO IBM C NFLX FSLR".Split(" ".ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
-
-            DoWork(symbols, a);
-
+            //done
+            Console.WriteLine("Download Complete!");
             Console.ReadKey();
         }
 
-        public static void DoWork(List<string> symbols, Action<string> downloader)
+        public static void DoWorkSingleThreaded(List<string> symbols, Action<string> downloader)
+        {
+
+            for (var i = 0; i < symbols.Count; i++)
+            {
+                var symbolTemp = symbols[i];
+
+                downloader(symbolTemp);
+            }
+        }
+
+        public static void DoWorkMultiThreaded(List<string> symbols, Action<string> downloader)
         {
 
             Task[] tasks = new Task[symbols.Count];
 
-            for (var i= 0; i < symbols.Count; i++)
+            for (var i = 0; i < symbols.Count; i++)
             {
                 var symbolTemp = symbols[i];
 
-                tasks[i] = (Task.Run(() =>
-                {
-                    Console.WriteLine($"Downloading: {symbolTemp}");
-
-                    semaphore.Wait();
-
-                    
-
-                    var wc = new WebClient();
-
-
-                    try
-                    {
-                        wc.DownloadFile(
-                        new Uri($@"https://ichart.finance.yahoo.com/table.csv?d=3&e=1&f=2017&g=d&a=1&b=1&c=2016&ignore=.csv&s={symbolTemp}"),
-                        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", $"{symbolTemp}.csv"));
-
-                        Console.WriteLine($"Completed: {symbolTemp}");
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"{ex}: {symbolTemp}");
-                    }
-                    finally
-                    {
-                        semaphore.Release();
-                    }
-                }));
+                tasks[i] = Task.Run(() => downloader(symbolTemp));
             }
 
-            semaphore.Release(2);
+            semaphore.Release(options.Threads);
             
             //semaphore.Release(3);
 
             Task.WaitAll(tasks);
         }
-
 
     }
 }
---
+++ YahooPriceDownloader/Utility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahooPriceDownloader
+{
+    public static class Utility
+    {
+        public static StreamReader ToStreamReader(this string str)
+        {
+            byte[] byteArray = Encoding.UTF8.GetBytes(str);
+            //byte[] byteArray = Encoding.ASCII.GetBytes(contents);
+            MemoryStream stream = new MemoryStream(byteArray);
+
+
+            // convert stream to string
+            return new StreamReader(stream);
+        }
+    }
+}
